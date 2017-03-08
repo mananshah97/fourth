d453c18eb628f389e7ec1ccfae4e5f8e55186593
mananshah97 <shah.manan99@gmail.com>
2017-03-08 12:50:40 +0530

    Update script3.js

--- site/script3.js
+++ site/script3.js
@@ -14,7 +14,8 @@ var result=q+w;
       this.textContent = "Said it!";
       var name =
        document.getElementById("name").value;
-       var message = "<h2>Hello " +name + "!</h2>";
+       var message = "<h2>Hello " + name + "!</h2>";
+
 
       document
         .getElementById("content")

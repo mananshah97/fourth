5c94f7a2f5d99a9e8653471308636992d4a9fe9f
mananshah97 <shah.manan99@gmail.com>
2017-03-11 15:49:19 +0530

    Update script3.js

--- site/script3.js
+++ site/script3.js
@@ -47,11 +47,13 @@ document.addEventListener("DOMContentLoaded",
   }
 );
 
-function add_number(){
-            var first_number = parseInt(document.getElementsById("Text1").value);
-            var second_number = parseInt(document.getElementsById("Text2").value);
-            var result = first_number + second_number;
-            document.getElementById("txtresult").innerHTML = result;    
+ function add_number() {
+               
+                var first_number = parseInt(document.getElementById("Text1").value);
+                var second_number = parseInt(document.getElementById("Text2").value);
+                var result = first_number + second_number;
+     
+                document.getElementById("txtresult").value = result;
             }
 
 // document.querySelector("button")

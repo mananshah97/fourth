e947e2a142affda8c3dfc79f0853f73435b7c753
mananshah97 <shah.manan99@gmail.com>
2017-03-22 12:19:51 +0530

    Update script.js

--- site/test/script.js
+++ site/test/script.js
@@ -13,13 +13,26 @@ document.addEventListener("DOMContentLoaded",
             function (res) {
 		console.log(res.name);
 		console.log(res.States);
-    var message="fd";
+    var message=" ";
+		var answer=" ";
               if (res.name === "Manan") {
-                message += "Done";
+		      message += " Done";
+		      if(res.States == 29){
+			      answer += "Correct Ans";
+		      }
+		      else
+		      {
+		      answer += "Wrong Answer";
+		      }
+		      
+			      
+			      
               }
 
               document.querySelector("#content")
                 .innerHTML = "<h2>" + message + "</h2";	
+		      document.querySelector("#content")
+                .innerHTML = "<h2>" + answer + "</h2";	
 });
 
 
